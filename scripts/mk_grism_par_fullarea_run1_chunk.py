cba56efb3564fe1e5d8b431fbb1d7ea7563be054
ashleyjross <ashley.jacob.ross@gmail.com>
2025-05-08 14:29:03 -0400

    Update mk_grism_par_fullarea_run1_chunk.py

--- scripts/mk_grism_par_fullarea_run1_chunk.py
+++ scripts/mk_grism_par_fullarea_run1_chunk.py
@@ -1,4 +1,6 @@
 #srun -N 1 -C cpu -t 04:00:00 --qos interactive --account m4943 python scripts/mk_grism_par_fullarea_run1_chunk.py
+#srun -N 1 -C cpu -t 04:00:00 --qos interactive --account m4943 python scripts/mk_grism_par_fullarea_run1_chunk.py --dith_ind 1
+#srun -N 1 -C cpu -t 04:00:00 --qos interactive --account m4943 python scripts/mk_grism_par_fullarea_run1_chunk.py --dith_ind 0 --dec_ind 1
 from astropy.table import Table
 from grism_sim import mk_ref_and_grism
 import os
@@ -38,7 +40,8 @@ decpa = 0.2 #how much offset there is in declination between 0 and 180 pa
 ndec = 4 #number of "rows"
 nra = 2 #number of "columns"
 
-pal = [0,5,175,180] #position angles
+pa_off = 60 #new code shifted by 60
+pal = [0+pa_off,5+pa_off,175+pa_off,180+pa_off] #position angles
 
 for det in range(0,ndet):
     for pa in pal:
