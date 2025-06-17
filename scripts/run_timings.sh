2c5ca9995c40a5a505a2b3d2e9b127115aa15736
Keith Buckholz <keith.buckholz@yale.edu>
2025-06-17 15:41:38 -0400

    fix job script - time limit

--- scripts/run_timings.sh
+++ scripts/run_timings.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=keith.buckholz@yale.edu
 #SBATCH --mail-type=ALL
 #SBATCH -A m4943
-#SBATCH -t 0:2:0
+#SBATCH -t 2:0:0
 #SBATCH -L cfs
 
 srun python /global/common/software/m4943/grizli0/grism_sim/scripts/mk_grism_timing.py
