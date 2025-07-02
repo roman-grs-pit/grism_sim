26224eaf08b7c4a1e6a9319afc00f95b08dc37cc
Keith Buckholz <keith.buckholz@yale.edu>
2025-07-02 18:48:28 -0400

    add stage_1 mk script and job script

---
+++ scripts/mk_stage_1.py
@@ -0,0 +1,35 @@
+from astropy.table import Table
+from grism_sim_psf_dependent import mk_grism
+import os
+from multiprocessing import Pool
+
+outdir = os.getenv("SCRATCH")
+
+stars = Table.read('/global/cfs/cdirs/m4943/grismsim/stars/sim_star_cat_galacticus.ecsv')
+# gals = Table.read('/global/cfs/cdirs/m4943/grismsim/galacticus_4deg2_mock/Euclid_Roman_4deg2_radec.fits')
+gals = None
+
+sel = stars["magnitude"] <= 17
+stars = stars[sel]
+
+def dosim(args, **kwargs):
+    print(kwargs)
+    mk_grism(args[0],args[1],args[2],args[3],args[4],args[5],args[6],
+             confver="03192025_rotAonly",check_psf=True, dogal='n',
+             conv_gal=False, npsfs=args[7],
+             **kwargs)
+
+ra = 10
+dec = 0
+pa = 60
+det_nums = range(1, 19)
+
+args = []
+for npsfs in [10, 20, 50]:
+    for det_num in det_nums:
+        args.append([ra,dec,pa,det_num,stars,gals,outdir,npsfs])
+
+print("will simulate"+str(len(args))+" grism images")
+
+with Pool(processes=54) as pool:
+    res = pool.map(dosim, args)
