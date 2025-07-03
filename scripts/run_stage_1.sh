0f77c9af0fd0883e58bd1b329567d401df7b0660
Keith Buckholz <keith.buckholz@yale.edu>
2025-07-02 23:06:53 -0400

    change name and time limit

--- scripts/run_stage_1.sh
+++ scripts/run_stage_1.sh
@@ -5,11 +5,11 @@
 #SBATCH -c 54
 #SBATCH -C cpu
 #SBATCH -q regular
-#SBATCH -J "Test stpsf and load_psf"
+#SBATCH -J "Stage 1 Simulation Run"
 #SBATCH --mail-user=keith.buckholz@yale.edu
 #SBATCH --mail-type=ALL
 #SBATCH -A m4943
-#SBATCH -t 0:2:0
+#SBATCH -t 2:0:0
 #SBATCH -L cfs
 
 srun python /global/common/software/m4943/grizli0/grism_sim/scripts/mk_stage_1.py
