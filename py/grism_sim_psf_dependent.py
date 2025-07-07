54501c8c641d20e2c7722e29dd3f7f08a7eab2f9
Keith Buckholz <keith.buckholz@yale.edu>
2025-07-07 13:14:15 -0400

    correct ERR HDU

--- py/grism_sim_psf_dependent.py
+++ py/grism_sim_psf_dependent.py
@@ -515,12 +515,12 @@ def mk_grism(tel_ra,tel_dec,pa,det_num,star_input,gal_input,output_dir,confver='
     if gpad != 0:
         hdu_list.append(fits.ImageHDU(data=true_noiseless[gpad:-gpad, gpad:-gpad], name='MODEL'))
         #hdu_list.append(fits.ImageHDU(data=roman.grism.data['SCI'][gpad:-gpad, gpad:-gpad],name='ERR'))
-        hdu_list['ERR'].data = bg_noise[gpad:-gpad, gpad:-gpad] + np.sqrt(full_model_noiseless[gpad:-gpad, gpad:-gpad])
+        hdu_list['ERR'].data = np.sqrt((full_model_noiseless[gpad:-gpad, gpad:-gpad] + background) * EXPTIME) / EXPTIME
         hdu_list["SCI"].data = full_model_final[gpad:-gpad, gpad:-gpad]
     else:
         hdu_list.append(fits.ImageHDU(data=true_noiseless, name='MODEL'))
         #hdu_list.append(fits.ImageHDU(data=roman.grism.data['SCI']),name='ERR')
-        hdu_list['ERR'].data = bg_noise + np.sqrt(full_model_noiseless)
+        hdu_list['ERR'].data = np.sqrt((full_model_noiseless + background) * EXPTIME) / EXPTIME
         hdu_list["SCI"].data = full_model_final
     
     out_fn = os.path.join(output_dir, fn_root_grism+'.fits')
