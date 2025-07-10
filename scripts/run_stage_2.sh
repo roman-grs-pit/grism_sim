452f2b004365e4e5b01b3838243706310a4d131c
Keith Buckholz <keith.buckholz@yale.edu>
2025-07-10 17:38:56 -0400

    increase max time in stage 2 job script

--- scripts/run_stage_2.sh
+++ scripts/run_stage_2.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=keith.buckholz@yale.edu
 #SBATCH --mail-type=ALL
 #SBATCH -A m4943
-#SBATCH -t 2:0:0
+#SBATCH -t 4:0:0
 #SBATCH -L cfs
 
 srun python /global/common/software/m4943/grizli0/grism_sim/scripts/mk_sim.py $SCRATCH/stage_2
