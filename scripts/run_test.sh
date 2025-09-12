45d02dcf62f415891a81cf10f935997fe998880d
Keith Buckholz <keith.buckholz@yale.edu>
2025-09-12 15:48:23 -0400

    update time limit

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=keith.buckholz@yale.edu
 #SBATCH --mail-type=ALL
 #SBATCH -A m4943
-#SBATCH -t 0:0:30
+#SBATCH -t 0:30:0
 #SBATCH -L cfs
 
 srun python /global/common/software/m4943/grizli0/grism_sim/tests/mk_visual_inspection_image.py
