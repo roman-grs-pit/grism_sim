f477ef533b3e5a84a25156e4ca223aee266d6fb4
Keith Buckholz <keith.buckholz@yale.edu>
2025-09-26 11:55:31 -0400

    bug fix - must append new ISIM_SCI grism hdu

--- py/grism_sim_psf_dependent.py
+++ py/grism_sim_psf_dependent.py
@@ -612,13 +612,13 @@ def mk_grism(wfi_cen_ra,wfi_cen_dec,wfi_cen_pa,det_num,star_input,gal_input,outp
         #hdu_list.append(fits.ImageHDU(data=roman.grism.data['SCI'][gpad:-gpad, gpad:-gpad],name='ERR'))
         hdu_list['ERR'].data = np.sqrt((full_model_noiseless[gpad:-gpad, gpad:-gpad] + background) * EXPTIME) / EXPTIME
         hdu_list["SCI"].data = full_model_final[gpad:-gpad, gpad:-gpad]
-        hdu_list["ISIM_SCI"] = full_model_poisson[gpad:-gpad, gpad:-gpad]
+        hdu_list.append(fits.ImageHDU(data=full_model_poisson[gpad:-gpad, gpad:-gpad], name="ISIM_SCI"))
     else:
         hdu_list.append(fits.ImageHDU(data=true_noiseless, name='MODEL'))
         #hdu_list.append(fits.ImageHDU(data=roman.grism.data['SCI']),name='ERR')
         hdu_list['ERR'].data = np.sqrt((full_model_noiseless + background) * EXPTIME) / EXPTIME
         hdu_list["SCI"].data = full_model_final
-        hdu_list["ISIM_SCI"] = full_model_poisson
+        hdu_list.append(fits.ImageHDU(data=full_model_poisson, name="ISIM_SCI"))
     
     out_fn = os.path.join(output_dir, fn_root_grism+'.fits')
     hdu_list.writeto(out_fn, overwrite=True)
