87440bca6e1c360946459895f5e65e1e986990f2
Keith Buckholz <keith.buckholz@yale.edu>
2025-12-04 20:07:48 -0500

    Add fits_to_asdf.sh
    Add bash script, which can be called inside other bash scripts to glob grism fits files and run the through romanisim to produce asdf files.

---
+++ scripts/fits_to_asdf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$1" ] # check if argument was provided
+then
+  echo No target directory provided. Please provide a directory containing fits files.
+  exit
+fi
+
+for fn in "$1"/grism_*_detSCA??.fits;
+do
+
+  fn_stripped=${fn%.*} # strip .fits from filename
+
+  det_num=${fn#*SCA} # strip anything preceding the detector number
+  det_num=${det_num%[._]*} # strip anything after the detector number
+
+  ra=${fn#*ra} # extract ra from filename
+  ra=${ra%_*}
+
+  dec=${fn#*dec} # extract ra from filename
+  dec=${dec%_*}
+
+  echo Processing "$fn" for detector SCA"$det_num"
+
+  det_num=${det_num#0} # strip leading zeros if single digit detector number
+
+  romanisim-make-image  --extra-counts "$fn" 5 \
+  --radec ra dec --date 2026-10-01T12:00:00.000 \
+  --sca "$det_num" --level 1 \
+  --pretend-spectral True --nobj 0 \
+  --bandpass F158 \
+  "${fn_stripped}_l1.asdf"
+
+  romanisim-make-image  --extra-counts "$fn" 5 \
+  --radec ra dec --date 2026-10-01T12:00:00.000 \
+  --sca "$det_num" --level 2 \
+  --pretend-spectral True --nobj 0 \
+  --bandpass F158 \
+  "${fn_stripped}_l2.asdf"
+
+done
