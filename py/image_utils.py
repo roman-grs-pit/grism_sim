6925d21d5f3bc76aa4a8a6075a65f75a7c0c9e2d
ashleyjross <ashley.jacob.ross@gmail.com>
2026-03-09 12:20:47 -0400

    Update image_utils.py

--- py/image_utils.py
+++ py/image_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 from astropy.io import fits
-import pysiaf
+
 
 try:
     import stpsf
@@ -8,31 +8,37 @@ except:
     import webbpsf as stpsf
 
 wfi = stpsf.roman.WFI()
-wfi.filter = "GRISM0" 
+wfi.filter = "GRISM0"
 
 _WFI_SIAF = None
 _DET_RA = None
 _DET_DEC = None
 
+
 def setup_pysiaf(wfi_cen_ra: float, wfi_cen_dec: float, wfi_cen_pa: float, det_num: int):
+    import pysiaf
     global _WFI_SIAF, _DET_RA, _DET_DEC
 
     siaf = pysiaf.Siaf("roman")
     wfi_siaf = siaf["WFI{:02}_FULL".format(det_num)]
-    
+
     # Use WFI_CEN for aiming
     v2ref = siaf["WFI_CEN"].V2Ref
     v3ref = siaf["WFI_CEN"].V3Ref
 
-    attmat = pysiaf.utils.rotations.attitude_matrix(v2ref, v3ref, wfi_cen_ra, wfi_cen_dec, wfi_cen_pa) # pysiaf wfi_cen_pa is 60 more than image_utils wfi_cen_pa (i.e. siaf_pa = iu_pa + 60)
+    # pysiaf wfi_cen_pa is 60 more than image_utils wfi_cen_pa (i.e. siaf_pa = iu_pa + 60)
+    attmat = pysiaf.utils.rotations.attitude_matrix(
+        v2ref, v3ref, wfi_cen_ra, wfi_cen_dec, wfi_cen_pa)
 
     wfi_siaf.set_attitude_matrix(attmat)
-    
+
     # cache as global variables
     _WFI_SIAF = wfi_siaf
-    _DET_RA, _DET_DEC = wfi_siaf.det_to_sky(2043, 2043) # I believe pysiaf uses 0-index for origin pixel; thus, center pix is 2043 not 2044
+    # I believe pysiaf uses 0-index for origin pixel; thus, center pix is 2043 not 2044
+    _DET_RA, _DET_DEC = wfi_siaf.det_to_sky(2043, 2043)
 
     return wfi_siaf
+
 
 def get_wfi_siaf(wfi_cen_ra: float, wfi_cen_dec: float, wfi_cen_pa: float, det_num: int):
     global _WFI_SIAF
@@ -42,13 +48,15 @@ def get_wfi_siaf(wfi_cen_ra: float, wfi_cen_dec: float, wfi_cen_pa: float, det_n
 
     return _WFI_SIAF
 
+
 def get_det_center(wfi_cen_ra: float, wfi_cen_dec: float, wfi_cen_pa: float, det_num: int):
     global _WFI_SIAF, _DET_RA, _DET_DEC
 
     if _WFI_SIAF is None:
         setup_pysiaf(wfi_cen_ra, wfi_cen_dec, wfi_cen_pa, det_num)
-    
+
     return _DET_RA, _DET_DEC
+
 
 def trim_catalog(catalog, wfi_cen_ra, wfi_cen_dec, wfi_cen_pa, det_num, gpad, tot_im_size,
                  col_names: dict = {"RA": "RA", "DEC": "DEC"}, initial_cut=2):
@@ -88,26 +96,31 @@ def trim_catalog(catalog, wfi_cen_ra, wfi_cen_dec, wfi_cen_pa, det_num, gpad, to
     wfi_siaf = get_wfi_siaf(wfi_cen_ra, wfi_cen_dec, wfi_cen_pa, det_num)
 
     # find xy detector coordinates
-    object_xy_siaf = wfi_siaf.sky_to_sci(trimmed_catalog[ra_col], trimmed_catalog[dec_col])
+    object_xy_siaf = wfi_siaf.sky_to_sci(
+        trimmed_catalog[ra_col], trimmed_catalog[dec_col])
     object_xy = (object_xy_siaf[0] + gpad, object_xy_siaf[1] + gpad)
-    
+
     trimmed_catalog.add_column(object_xy[0], name="det_x")
     trimmed_catalog.add_column(object_xy[1], name="det_y")
 
     # mask off-detector objects
-    sel_ondet = object_xy[0] > 0#stars00['Xpos'] < 4088 + 2*( gpad) #we want everything within padded area around grism
+    # stars00['Xpos'] < 4088 + 2*( gpad) #we want everything within padded area around grism
+    sel_ondet = object_xy[0] > 0
     sel_ondet &= object_xy[0] < tot_im_size
-    sel_ondet &= object_xy[1] > 0#stars00['Xpos'] < 4088 + 2*( gpad) #we want everything within padded area around grism
+    # stars00['Xpos'] < 4088 + 2*( gpad) #we want everything within padded area around grism
+    sel_ondet &= object_xy[1] > 0
     sel_ondet &= object_xy[1] < tot_im_size
 
     trimmed_catalog = trimmed_catalog[sel_ondet]
 
     return trimmed_catalog
 
-# We are currently using an older version of photutils where a few of the PSF grid functions 
+# We are currently using an older version of photutils where a few of the PSF grid functions
 # are not available.
 # We therefore duplicate these here. At some point we should see if we can update photutils
 # and deprecate these.
+
+
 def _v2_find_bounding_points(psf_grid, x, y):
     """Find the grid points surrounding the point (x0, y0)."""
     xgrid = psf_grid._xgrid
@@ -139,6 +152,7 @@ def _v2_find_bounding_points(psf_grid, x, y):
 
     return grid_idx, grid_xy
 
+
 def _v2_calc_bilinear_weights(x, y, grid_xy):
     """Calculate the bilinear interpolation weights for the point (x, y) based on the bounding grid points."""
     x0, x1, y0, y1 = grid_xy
@@ -154,8 +168,10 @@ def _v2_calc_bilinear_weights(x, y, grid_xy):
 
     return weights
 
-# My original plan was to rewrite the gridded PSF class, but instead I think I can just hack 
+# My original plan was to rewrite the gridded PSF class, but instead I think I can just hack
 # this on, using the built in terms. If that is too slow, we can always try other approaches.
+
+
 def psf_grid_evaluate_fast(psf_grid, x0, y0, mag):
     """
     Evaluate the PSF at a specific point using a given GriddedPSFModel object.
@@ -177,7 +193,7 @@ def psf_grid_evaluate_fast(psf_grid, x0, y0, mag):
         flux = mag2flux(mag)
     else:
         flux = 1
-    
+
     grid_idx, grid_xy = _v2_find_bounding_points(psf_grid, x0, y0)
     weights = _v2_calc_bilinear_weights(x0, y0, grid_xy)
 
@@ -186,6 +202,7 @@ def psf_grid_evaluate_fast(psf_grid, x0, y0, mag):
         result += weight * psf_grid.data[idx]
 
     return result * flux
+
 
 def star_postage_grid(psf_grid, mag, detx=2044, dety=2044, half_fov_pixels=182):
     """
@@ -210,10 +227,13 @@ def star_postage_grid(psf_grid, mag, detx=2044, dety=2044, half_fov_pixels=182):
 
     x_0 = int(detx)
     y_0 = int(dety)
-    y, x = np.mgrid[y_0-half_fov_pixels:y_0+half_fov_pixels, x_0-half_fov_pixels:x_0+half_fov_pixels]
+    y, x = np.mgrid[y_0-half_fov_pixels:y_0+half_fov_pixels,
+                    x_0-half_fov_pixels:x_0+half_fov_pixels]
 
-    psf = psf_grid.evaluate(x=x, y=y, x_0=detx, y_0=dety, flux=flux).astype(np.float32)
+    psf = psf_grid.evaluate(x=x, y=y, x_0=detx, y_0=dety,
+                            flux=flux).astype(np.float32)
     return psf
+
 
 def gal_postage_grid(psf_grid, detx=2044, dety=2044, half_fov_pixels=364, flux=1):
     """
@@ -234,10 +254,13 @@ def gal_postage_grid(psf_grid, detx=2044, dety=2044, half_fov_pixels=364, flux=1
 
     x_0 = int(detx)
     y_0 = int(dety)
-    y, x = np.mgrid[y_0-half_fov_pixels:y_0+half_fov_pixels, x_0-half_fov_pixels:x_0+half_fov_pixels]
+    y, x = np.mgrid[y_0-half_fov_pixels:y_0+half_fov_pixels,
+                    x_0-half_fov_pixels:x_0+half_fov_pixels]
 
-    psf = psf_grid.evaluate(x=x, y=y, x_0=detx, y_0=dety, flux=flux) #? What's the right flux for this?
+    # ? What's the right flux for this?
+    psf = psf_grid.evaluate(x=x, y=y, x_0=detx, y_0=dety, flux=flux)
     return psf
+
 
 def create_psf_grid(wavelength=1.5e-6, fov_pixels=364, det="SCA01"):
     """
@@ -255,11 +278,14 @@ def create_psf_grid(wavelength=1.5e-6, fov_pixels=364, det="SCA01"):
     """
 
     wfi.detector = det
-    grid = wfi.psf_grid(all_detectors=False, use_detsampled_psf=True, monochromatic=wavelength, fov_pixels=fov_pixels)
+    grid = wfi.psf_grid(all_detectors=False, use_detsampled_psf=True,
+                        monochromatic=wavelength, fov_pixels=fov_pixels)
     return grid
 
-#fiducial zero point set based on 2022 sim below
-def mag2flux(mag,zp=26.5):
+# fiducial zero point set based on 2022 sim below
+
+
+def mag2flux(mag, zp=26.5):
     """
     Convert magnitude to flux.
     returns flux observed by detector: float
@@ -273,10 +299,11 @@ def mag2flux(mag,zp=26.5):
     """
 
     f0 = 10**(0.4*zp)
-    flux = f0*10**(-0.4*mag) #mag = 26.5 - 2.5*np.log10(sumflux)
+    flux = f0*10**(-0.4*mag)  # mag = 26.5 - 2.5*np.log10(sumflux)
     return flux
-    
-def star_postage(mag,detx=2044,dety=2044,offx=0,offy=0,wavelength = 1.5e-6, fov_pixels=364, oversample=4,arcperpixel = 0.11, det="SCA01"):
+
+
+def star_postage(mag, detx=2044, dety=2044, offx=0, offy=0, wavelength=1.5e-6, fov_pixels=364, oversample=4, arcperpixel=0.11, det="SCA01"):
     """
     Evaluate monochromatic PSF for a given detector at a given detector coordinate. 
     Total thumbnail size in pixel is fov_pixels * oversample.
@@ -313,11 +340,13 @@ def star_postage(mag,detx=2044,dety=2044,offx=0,offy=0,wavelength = 1.5e-6, fov_
     wfi.options['source_offset_y'] = offy*arcperpixel
     wfi.detector = det
     wfi.detector_position = (detx, dety)
-    psf = wfi.calc_psf(monochromatic=wavelength, fov_pixels=fov_pixels, oversample=oversample)
+    psf = wfi.calc_psf(monochromatic=wavelength,
+                       fov_pixels=fov_pixels, oversample=oversample)
     flux = mag2flux(mag)
     return psf[0].data*flux
 
-def get_psf(wavelength = 1.5e-6, fov_pixels=364, oversample=4,detx=2044,dety=2044, det="SCA01"):
+
+def get_psf(wavelength=1.5e-6, fov_pixels=364, oversample=4, detx=2044, dety=2044, det="SCA01"):
     """
     Evaluate monochromatic PSF for a given detector at a given detector coordinate. 
     Total thumbnail size in pixel is fov_pixels * oversample. This function does
@@ -345,12 +374,15 @@ def get_psf(wavelength = 1.5e-6, fov_pixels=364, oversample=4,detx=2044,dety=204
     """
 
     wfi.detector = det
-    wfi.detector_position = (detx, dety) #fiducial case is at the center
-    psf = wfi.calc_psf(monochromatic=wavelength, fov_pixels=fov_pixels, oversample=oversample)
+    wfi.detector_position = (detx, dety)  # fiducial case is at the center
+    psf = wfi.calc_psf(monochromatic=wavelength,
+                       fov_pixels=fov_pixels, oversample=oversample)
     return psf
 
-#wavelength = 1.5e-6, fov_pixels=364, oversample=2
-def star_postage_inpsf(mag,psf):
+# wavelength = 1.5e-6, fov_pixels=364, oversample=2
+
+
+def star_postage_inpsf(mag, psf):
     """
     Adjust a fiducial PSF to given magnitude.
     returns psf thumbnail: numpy.ndarray
@@ -366,7 +398,7 @@ def star_postage_inpsf(mag,psf):
     return psf[0].data*flux
 
 
-def add_Roman_header(file_name,fout_name=None,roman_base_dir='',det='1'):
+def add_Roman_header(file_name, fout_name=None, roman_base_dir='', det='1'):
     """
     DEPRECATED. This function is not longer used in current scripts.
 
@@ -388,15 +420,17 @@ def add_Roman_header(file_name,fout_name=None,roman_base_dir='',det='1'):
 
     if fout_name is None:
         fout_name = file_name
-    file = fits.open(file_name) 
+    file = fits.open(file_name)
     file[0].header["INSTRUME"] = "ROMAN"
     file[0].header["FILTER"] = "d1_"
-    file[1].header["CONFFILE"] = roman_base_dir+"configuration/Roman.det"+det+".07242020.conf"
+    file[1].header["CONFFILE"] = roman_base_dir + \
+        "configuration/Roman.det"+det+".07242020.conf"
     file.writeto(fout_name, overwrite=True)
     file.close()
 
-def fake_header_wcs(crval1, crval2, crpix2=2044,crpix1=2044, cdelt1=0.11, cdelt2=0.11,
-                crota2=0.0,naxis1=4088,naxis2=4088):
+
+def fake_header_wcs(crval1, crval2, crpix2=2044, crpix1=2044, cdelt1=0.11, cdelt2=0.11,
+                    crota2=0.0, naxis1=4088, naxis2=4088):
     """
     Returns an otherwise empty PrimaryHDU containing only WCS information.
 
@@ -407,15 +441,15 @@ def fake_header_wcs(crval1, crval2, crpix2=2044,crpix1=2044, cdelt1=0.11, cdelt2
     """
 
     # NOTE: Pysiaf is used for WCS now. These functions should only be used for fits header info
-    #make empty hdu header and add wcs 
-    
+    # make empty hdu header and add wcs
+
     hdu = fits.PrimaryHDU()
-    
-    return add_wcs(hdu,crval1, crval2, crpix2,crpix1, cdelt1, cdelt2, crota2,naxis1,naxis2)
+
+    return add_wcs(hdu, crval1, crval2, crpix2, crpix1, cdelt1, cdelt2, crota2, naxis1, naxis2)
 
 
-def add_wcs(hdu,crval1, crval2, crpix2=2044,crpix1=2044, cdelt1=0.11, cdelt2=0.11,
-                crota2=0.0,naxis1=4088,naxis2=4088):
+def add_wcs(hdu, crval1, crval2, crpix2=2044, crpix1=2044, cdelt1=0.11, cdelt2=0.11,
+            crota2=0.0, naxis1=4088, naxis2=4088):
     """
     Adds WCS information to given HDU header
     returns hdu.header: astropy.io.fits.header.Header
@@ -427,50 +461,49 @@ def add_wcs(hdu,crval1, crval2, crpix2=2044,crpix1=2044, cdelt1=0.11, cdelt2=0.1
     crval2: float
     """
     # NOTE: Pysiaf is used for WCS now. These functions should only be used for fits header info
-    
-    #add wcs to existing header
-    #maintain consistency with https://github.com/roman-grs-pit/observing-program/blob/main/py/footprintutils.py, at some point make both use same function
+
+    # add wcs to existing header
+    # maintain consistency with https://github.com/roman-grs-pit/observing-program/blob/main/py/footprintutils.py, at some point make both use same function
     # crota2 - degree
     # cdelt1 - arcsec
     # cdelt2 - arcsec
 
-    #hdu = fits.PrimaryHDU()
-    #hdu.header
+    # hdu = fits.PrimaryHDU()
+    # hdu.header
 
     # http://stsdas.stsci.edu/documents/SUG/UG_21.html
 
-    theta = crota2*np.pi/180. # radians
-    cdelt1 /= 3600. # deg
-    cdelt2 /= 3600. # deg
+    theta = crota2*np.pi/180.  # radians
+    cdelt1 /= 3600.  # deg
+    cdelt2 /= 3600.  # deg
 
     R = np.array([
         [-1*np.cos(theta), 1*np.sin(theta)],
         [1*np.sin(theta), np.cos(theta)],
     ])
 
+    cd1_1 = cdelt1*R[0, 0]
+    cd1_2 = cdelt2*R[0, 1]
+    cd2_1 = cdelt1*R[1, 0]
+    cd2_2 = cdelt2*R[1, 1]
 
-    cd1_1 = cdelt1*R[0,0]
-    cd1_2 = cdelt2*R[0,1]
-    cd2_1 = cdelt1*R[1,0]
-    cd2_2 = cdelt2*R[1,1]
-                    
-    hdu.header.set('NAXIS',2) # pixels
-    hdu.header.set('NAXIS1',naxis1) # pixels
-    hdu.header.set('NAXIS2',naxis2) # pixels
+    hdu.header.set('NAXIS', 2)  # pixels
+    hdu.header.set('NAXIS1', naxis1)  # pixels
+    hdu.header.set('NAXIS2', naxis2)  # pixels
 
-    hdu.header.set('WCSAXES',2) # pixels
-    hdu.header.set('CTYPE1','RA---TAN')
-    hdu.header.set('CTYPE2','DEC--TAN')
-    hdu.header.set('CRVAL1',crval1) # deg
-    hdu.header.set('CRVAL2',crval2) # deg
-    hdu.header.set('CRPIX1',crpix1) # pixels
-    hdu.header.set('CRPIX2',crpix2) # pixels
-    #hdu.header.set('CDELT1',cdelt1)
-    #hdu.header.set('CDELT2',cdelt2)
-    #hdu.header.set('CROTA2',crota2)
-    hdu.header.set("CD1_1",cd1_1)
-    hdu.header.set("CD1_2",cd1_2)
-    hdu.header.set("CD2_1",cd2_1)
-    hdu.header.set("CD2_2",cd2_2)
+    hdu.header.set('WCSAXES', 2)  # pixels
+    hdu.header.set('CTYPE1', 'RA---TAN')
+    hdu.header.set('CTYPE2', 'DEC--TAN')
+    hdu.header.set('CRVAL1', crval1)  # deg
+    hdu.header.set('CRVAL2', crval2)  # deg
+    hdu.header.set('CRPIX1', crpix1)  # pixels
+    hdu.header.set('CRPIX2', crpix2)  # pixels
+    # hdu.header.set('CDELT1',cdelt1)
+    # hdu.header.set('CDELT2',cdelt2)
+    # hdu.header.set('CROTA2',crota2)
+    hdu.header.set("CD1_1", cd1_1)
+    hdu.header.set("CD1_2", cd1_2)
+    hdu.header.set("CD2_1", cd2_1)
+    hdu.header.set("CD2_2", cd2_2)
 
     return hdu.header
